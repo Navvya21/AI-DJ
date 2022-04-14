c71949b2602cbbe141c59795cd26a92a28cdb3dc
Navvya21 <75784090+Navvya21@users.noreply.github.com>
2022-04-14 13:42:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,9 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY); 
@@ -39,6 +42,16 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("FF0000");
+if(ScoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);}
 }
 
 function play()
